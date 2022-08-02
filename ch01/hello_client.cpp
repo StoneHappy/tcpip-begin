8c7a32631487c82161a58e1db933afadbf27c44d
StoneHappy <18017158729@163.com>
2022-08-02 17:58:05 +0800

    hello client

--- ch01/hello_client.cpp
+++ ch01/hello_client.cpp
@@ -1,6 +1,10 @@
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void error_hannding(const char* message);
 
 int main(int argc, char** argv)
 {
@@ -13,4 +17,28 @@ int main(int argc, char** argv)
         printf("Usage : %s <IP> <port>\n", argv[0]);
         exit(1);
     }
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    if(sock == -1)
+        error_hannding("socket() error");        
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_port = htons(atoi(argv[2]));
+    if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
+        error_hannding("connect() error");
+
+    str_len = read(sock, message, sizeof(message)-1);
+    if(str_len == -1)
+        error_hannding("read() error");
+    printf("Message from server : %s \n", message);
+    close(sock);
+    return 0;
+}
+
+
+void error_hannding(const char* message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
 }
